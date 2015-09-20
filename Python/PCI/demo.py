40754a977f8c073588037b472ddb1ac6e9662ce6
chenyang <168762116@qq.com>
2015-09-20 22:04:08 +0800

    col

---
+++ Python/PCI/demo.py
@@ -0,0 +1,7 @@
+from collaborativeFiltering import critics
+import collaborativeFiltering
+a = collaborativeFiltering.sim_distance(collaborativeFiltering.critics,
+                                    'Lisa Rose',
+                                    'Gene Seymour')
+print(a)
+
