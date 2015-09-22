e78f96b7651b378667209043707fa72ce4497444
chenyang <168762116@qq.com>
2015-09-22 21:57:22 +0800

    CH3

--- Python/PCI/C2/demo.py
+++ Python/PCI/C2/demo.py
@@ -1,10 +1,12 @@
-from collaborativeFiltering import critics
-import collaborativeFiltering
-a = collaborativeFiltering.sim_distance(collaborativeFiltering.critics,
+from recommendation import critics
+import recommendation
+import pydelicious
+from deliciousrec import *
+a = recommendation.sim_distance(recommendation.critics,
                                     'Lisa Rose',
                                     'Gene Seymour')
 print(a)
-a = collaborativeFiltering.topMatches(collaborativeFiltering.critics,
+a = recommendation.topMatches(recommendation.critics,
                                       'Toby', n = 3)
 print(a)
 
@@ -17,6 +19,7 @@ for i in b:
     b['ha'] += 1
     b.setdefault('ha', 2)
 print(b['ha'])
+
 #test for in in dictionary..
 set = {'a':0,'b':1,'c':2}
 res = [(val/0.5,key) for key,val in set.items()]
