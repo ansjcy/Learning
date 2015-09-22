390df632ae7baa92a589dbdcb295bfba171b0fb8
chenyang <168762116@qq.com>
2015-09-22 14:28:48 +0800

    recommendation system

--- Python/PCI/demo.py
+++ Python/PCI/demo.py
@@ -4,4 +4,24 @@ a = collaborativeFiltering.sim_distance(collaborativeFiltering.critics,
                                     'Lisa Rose',
                                     'Gene Seymour')
 print(a)
+a = collaborativeFiltering.topMatches(collaborativeFiltering.critics,
+                                      'Toby', n = 3)
+print(a)
+
+#test function setdefault..
+b = {}
+b.setdefault('ha', 2)
+b['xi'] = 3
+for i in b:
+    print(i)
+    b['ha'] += 1
+    b.setdefault('ha', 2)
+print(b['ha'])
+#test for in in dictionary..
+set = {'a':0,'b':1,'c':2}
+res = [(val/0.5,key) for key,val in set.items()]
+res.sort()
+print(res)
+
+
 
